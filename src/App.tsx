4e9cea60bb00a28826dcc04893920cf50527b27b
Suraj B M <silicoflare@gmail.com>
2024-10-14 11:58:26 +0530

    init: inital commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex flex-col w-screen h-screen justify-center items-center gap-2">
+      <h1 className="text-5xl font-bold">Hello World!!</h1>
+      <span className="font-mono">I am Suraj B M, the owner of this repo</span>
+    </div>
+  );
+}
