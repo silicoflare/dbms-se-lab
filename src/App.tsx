759cf8ea4b93183fa6e8e974f9ad2bc7cc25bc4d
tievenr <vineetraghu17@gmail.com>
2024-10-14 12:19:53 +0530

    change bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-export default function App() {
+export default function App()
+{
   return (
-    <div className="flex flex-col w-screen h-screen justify-center items-center gap-2">
+    <div className="flex flex-col w-screen h-screen justify-center items-center gap-2 bg-blue-500 text-white">
       <h1 className="text-5xl font-bold">Hello World!!</h1>
       <span className="font-mono">I am Suraj B M, the owner of this repo</span>
       <span className="font-serif">I am Vineet R, the collaborator of this repo</span>
